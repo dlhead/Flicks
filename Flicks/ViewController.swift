639884dbaf0bc7c14af305605b95e3178bfcdaf1
David Head <dhead2010@gmail.com>
2017-01-23 04:36:50 -0800

    Initial Commit

---
+++ Flicks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flicks
+//
+//  Created by David Head on 1/23/17.
+//  Copyright © 2017 dlhead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
