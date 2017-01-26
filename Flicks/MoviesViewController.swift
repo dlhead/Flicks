3465ca0c51717ef35dc7fba0e63cb3c223079844
David Head <dhead2010@gmail.com>
2017-01-25 21:45:54 -0800

    Successful build of Assignment 1 Part 2

--- Flicks/MoviesViewController.swift
+++ Flicks/MoviesViewController.swift
@@ -12,11 +12,33 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     @IBOutlet weak var tableView: UITableView!
     
+    // Instance variable for storing array of dictionaries returned from TMDB API. Each 
+    // dictionary includes data about a single movie.
+    var movies : [NSDictionary]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                    
+                    self.movies = dataDictionary["results"] as? [NSDictionary]
+                    // tell Table View to reload its data after the network request has been made
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        task.resume()
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,16 +47,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
     }
     // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier (from UITableViewDataSource protocol)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
         // Add a debug statement as a sanity check
-        cell.textLabel?.text = "row \(indexPath.row)"
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        cell.textLabel?.text = title
+
         print("row \(indexPath.row)")
         return cell
-        }
+    }
     
 
     /*
@@ -46,5 +75,4 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Pass the selected object to the new view controller.
     }
     */
-
 }
